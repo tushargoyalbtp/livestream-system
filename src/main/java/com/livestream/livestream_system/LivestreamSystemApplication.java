65d0684bef9b1c33bf47a283f85a5b8a3dfc30bd
tushargoyal <tushargoyalbtp2000@gmail.com>
2025-09-21 10:26:33 +0530

    Initialization of a project for a live stream user systems

---
+++ src/main/java/com/livestream/livestream_system/LivestreamSystemApplication.java
@@ -0,0 +1,13 @@
+package com.livestream.livestream_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LivestreamSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LivestreamSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/livestream/livestream_system/LivestreamSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.livestream.livestream_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LivestreamSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
